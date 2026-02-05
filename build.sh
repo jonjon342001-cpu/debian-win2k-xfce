6c6e422de50a4981b4c0c4bbbbda98c12c13a2cf
jonjon342001-cpu <jonjon342001@gmail.com>
2026-02-05 16:24:18 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,17 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y live-build git wget
 
+# Clean old builds
 lb clean
+
+# Configure the ISO (minimal working flags)
 lb config \
   --distribution bookworm \
   --debian-installer live \
   --archive-areas "main contrib non-free non-free-firmware" \
   --binary-images iso-hybrid \
   --iso-application "Debian Windows 2000 Clone" \
-  --iso-publisher "Win2k Linux Project" \
-  --package-lists "minimal,win2k"
+  --iso-publisher "Win2k Linux Project"
 
+# Build the ISO
 lb build
