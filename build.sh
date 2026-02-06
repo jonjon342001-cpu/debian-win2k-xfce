82ed1430cc5f6d9ff52a60c3013e002e63aa61a3
jonjon342001-cpu <jonjon342001@gmail.com>
2026-02-06 12:50:13 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,19 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt install -y live-build git wget
+apt install -y live-build debian-archive-keyring
 
 lb clean
 
 lb config \
+  --mode debian \
   --distribution bookworm \
   --debian-installer live \
   --archive-areas "main contrib non-free non-free-firmware" \
   --binary-images iso-hybrid \
+  --mirror-bootstrap http://deb.debian.org/debian \
+  --mirror-chroot http://deb.debian.org/debian \
+  --mirror-binary http://deb.debian.org/debian \
   --iso-application "Debian Windows 2000 Clone" \
   --iso-publisher "Win2k Linux Project"
 
